9d6c966112a552c6f8e4526c5db38590e9d80016
quannt <nguyentrongquan27112003@gmail.com>
2025-06-17 04:28:11 +0700

    external storage

--- app/src/main/java/com/he176875/myapplication/internalstorage/MainActivity.java
+++ app/src/main/java/com/he176875/myapplication/internalstorage/MainActivity.java
@@ -1,29 +1,171 @@
 package com.he176875.myapplication.internalstorage;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.material.button.MaterialButton;
 import com.he176875.myapplication.R;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int STORAGE_PERMISSION_CODE = 100;
+    private static final String TAG = "PERMISSION_TAG";
+
+    private EditText folderNameEt;
+    private MaterialButton createFolderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //init UI views
+        folderNameEt = findViewById(R.id.folderNameEt);
+        createFolderBtn = findViewById(R.id.createFolderBtn);
+
+        //handle click
+        createFolderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkPermission()) {
+                    Log.d(TAG, "onClick: Permissions already granted...");
+                    createFolder();
+                } else {
+                    Log.d(TAG, "onClick: Permissions was not granted, request...");
+                    requestPermission();
+                }
+            }
         });
     }
 
+    private void createFolder(){
+        //get folder name
+        String folderName = folderNameEt.getText().toString().trim();
+
+        //create folder using name just input
+        File file = new File(Environment.getExternalStorageDirectory() + "/" + folderName);
+
+        //create folder
+        boolean folderCreated = file.mkdir();
+
+        //show if folder created or not
+        if (folderCreated) {
+            Toast.makeText(this, "Folder Created...\n" + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Folder not created...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Android 11 trở lên (API 30+)
+            try {
+                Log.d(TAG, "requestPermission: try");
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+                intent.setData(uri);
+                storageActivityResultLauncher.launch(intent);
+            } catch (Exception e) {
+                Log.e(TAG, "requestPermission: catch", e);
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                storageActivityResultLauncher.launch(intent);
+            }
+        } else {
+            // Android 10 trở xuống
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                    },
+                    STORAGE_PERMISSION_CODE
+            );
+        }
+    }
+
+    private ActivityResultLauncher<Intent> storageActivityResultLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            Log.d(TAG, "onActivityResult: ");
+                            // here we will handle the result of our intent
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                                // Android is 11(R) or above
+                                //manage enternal storage permission is granted
+                                Log.d(TAG, "onActivityResult: Manage External Storage Permission is granted");
+                                createFolder();
+                            } else {
+                                // Android is below 11(R)
+                                Log.d(TAG, "onActivityResult: Manage External Storage Permission is denied");
+                                Toast.makeText(MainActivity.this, "Manage External Storage Permission is denied", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    }
+            );
+
+
+    public boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Android 11 trở lên
+            return Environment.isExternalStorageManager();
+        } else {
+            // Android 10 trở xuống
+            int write = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            return write == PackageManager.PERMISSION_GRANTED &&
+                    read == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+
+    /*Handle permission request results*/
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0) {
+                // check each permission if granted or not
+                boolean write = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean read = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (write && read) {
+                    // External Storage permissions granted
+                    Log.d(TAG, "onRequestPermissionsResult: External Storage permissions granted");
+                    createFolder();
+                } else {
+                    // External Storage permission denied
+                    Log.d(TAG, "onRequestPermissionsResult: External Storage permission denied");
+                    Toast.makeText(this, "External Storage permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
 
 }
